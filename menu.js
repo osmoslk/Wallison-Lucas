f7520ee3b752cd4bccd5064a447dee657200ccd8
wallisonsantos <wallisonluckas@gmail.com>
2025-07-03 18:36:37 -0300

    Deploy Inicial

---
+++ menu.js
@@ -0,0 +1,13 @@
+// menu.js
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("menu").innerHTML = `
+    <nav>
+      <ul>
+        <li><a href="index.html">Sobre mim</a></li>
+        <li><a href="formacao.html">Formação</a></li>
+        <li><a href="portfolio.html">Portfólio</a></li>
+        <li><a href="contato.html">Contato</a></li>
+      </ul>
+    </nav>
+  `;
+});
